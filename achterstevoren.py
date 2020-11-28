b1548220fdedefc4b0d11f8b2679c39ef0a73f2c
mithrandil444 <sven.devisscher@student.kdg.be>
2020-11-28 14:38:31 +0100

    versie 1.1

---
+++ achterstevoren.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""
+Python script dat je om een woord vraagt  en het achterstrevoren weergeeft.
+bron : https://www.w3schools.com/python/python_howto_reverse_string.asp
+"""
+
+
+__author__ = "Sven De Visscher"
+__email__ = "sven.devisscher@student.kdg.be"
+__status__ = "Development"
+
+
+# CONFIGURING I/O
+woord = input("typ hier een woord:  ") [::-1]   # Input dat vraagt of je een woord wilt ingeven
+""" [::-1] zorgt ervoor dat je ingegeven woord achterstevoren wordt uitgelezen. """
+
+
+def main():
+    print(woord)    # Print het ingegeven woord in de console
+
+
+if __name__ == '__main__':  # code to execute if called from command-line
+    main()
