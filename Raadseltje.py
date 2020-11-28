9e7b71c3a942e709b4713d3bb5530d0da2d5b114
mithrandil444 <sven.devisscher@student.kdg.be>
2020-11-28 15:39:50 +0100

    versie 1.1

--- Raadseltje.py
+++ Raadseltje.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python
 """
-Info about our project comes here
+Python script dat je vraagt naar een woord die in de lijst staat.
 """
 
 
 __author__ = "Sven De Visscher"
 __email__ = "sven.devisscher@student.kdg.be"
-__status__ = "Development"
+__status__ = "Finished"
 
 
 # Variabelen
-sporten = ["voetbal", "skiën", "squash", "tennis", "zwemmen"]
-guess = input("Enter guess: ")
+sporten = ["voetbal", "skiën", "squash", "tennis", "zwemmen", "pingpong"]   # Lijst met sporten
+hint = "welke sporten vind miranda leuk?"   # variabele genaamd hint
 
 
 def main():
-    if guess in sporten:
+    print(hint)                         # Print de hint
+    guess = input("Enter guess: ")      # Input dat ervoor zorgt dat je uw gok kan invoeren
+
+    if guess in sporten:                # Als het ingegeven woord in de lijst staat print het You win
         print("You win")
 
-    else:
+    else:                               # Als het ingegeven woord niet in de lijst staat print het You lose
         print("You lose")
-
-
 
 if __name__ == '__main__':  # code to execute if called from command-line
     main()
