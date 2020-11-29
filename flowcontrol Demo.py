13433f52869ae6782823d1f7546683c6f7eb09af
mithrandil444 <sven.devisscher@student.kdg.be>
2020-11-29 16:47:17 +0100

    versie 1.6

---
+++ flowcontrol Demo.py
@@ -0,0 +1,2 @@
+getal = input("Enter a number here: ") [::-1]
+print(getal)
--- rastershizzle.py
+++ rastershizzle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Info about our project comes here
+Python script dat ervoor zorgt dat de matrix  90° en 270° draait en wordt weergeven op de console.
 """
 
 # IMPORTS
@@ -8,15 +8,51 @@ Info about our project comes here
 
 __author__ = "Sven De Visscher"
 __email__ = "sven.devisscher@student.kdg.be"
-__status__ = "Development"
-
+__status__ = "Finished"
 
 # CONFIGURING I/O
+raster = [['.', '.', '.', '.', '.', '.'],
+          ['.', 'O', 'O', '.', '.', '.'],
+          ['O', 'O', 'O', 'O', '.', '.'],
+          ['O', 'O', 'O', 'O', 'O', '.'],
+          ['.', 'O', 'O', 'O', 'O', 'O'],
+          ['O', 'O', 'O', 'O', 'O', '.'],
+          ['O', 'O', 'O', 'O', '.', '.'],
+          ['.', 'O', 'O', '.', '.', '.'],
+          ['.', '.', '.', '.', '.', '.']]
 
 
 def main():
-    pass
+    rotated = rotate90(raster)  # 90°
+    rotated = rotate90(rotated)  # 180°
+    rotated = rotate90(rotated)  # 270°
+    printList(rotated)
+    print("\n")
+    rotated2 = rotate90(raster)  # 90°
+    printList(rotated2)
 
 
-if __name__ == '__main__':  # code to execute if called from command-line
+def rotate90(original):
+    r = len(original)  # Aantal rijen orignele matrix
+    c = len(original[0])  # Aantal kolommen orignele matrix
+    rotated = [[j for j in range(r)] for i in range(c)]  # Nieuwe matrix met c (rijen) en r (kolommen)
+    for x in range(c):
+        for y in range(r):
+            rotated[x][y] = original[y][(c - (x + 1))]
+        # Waarde originele matrix kopiëerde naar positie - 90° nieuwe matrix
+    return rotated  # Geroteerde matrix teruggeven aan main
+
+
+def printList(l):  # Lijst uitprinten in matrixvorm
+    # Aantal rijen en kolommen bepalen
+    r = len(l)
+    c = len(l[0])
+    for x in range(r):
+
+        for y in range(c):
+            print(l[x][y], end='')  # voorkomt 'enter' op einde print instructie
+        print("\n")
+
+
+if __name__ == '__main__':  # run tests if called from command-line
     main()
